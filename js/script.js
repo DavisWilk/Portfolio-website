2b2e7b1184df7bf00f2a99cca23c01cd2314133f
DavisWilk <76902777+DavisWilk@users.noreply.github.com>
2022-07-06 15:11:13 +0800

    Updates from usability Testing

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ console.log("welcome to my portfolio site")
 function menuToggle(){
     console.log('wtf')
     var x = document.getElementById('myNavToggle')
-    if(x.className === 'navtoggle'){
+    if(x.className === 'navtoggle' && $(window).width() < 640){
         x.className += ' responsive'
     } else {
         x.className = 'navtoggle'
